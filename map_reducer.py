500fb77ca7af896c18884396c82bba1964a92c72
scais <stepan.cais@gmail.com>
2017-06-05 19:59:31 +0100

    Created simple objects for holding the representation of object map entries

--- map_reducer.py
+++ map_reducer.py
@@ -1,6 +1,37 @@
 import argparse
 
 
+class SquishObject:
+
+    class Attribute:
+        def __init__(self, name, value, matching_type='='):
+            self.name = name
+            self.value = value
+            self.matching_type = matching_type
+
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+
+    @staticmethod
+    def create_squish_object(object_map_line):
+        # TODO: Create SquishObject from a line of an object map
+        return SquishObject(None, None)
+
+
+class ObjectMapExtractor:
+
+    object_map_file_path = ''
+
+    def __init__(self, object_map_file_path):
+        self.object_map_file_path = object_map_file_path
+
+    def extract_objects(self):
+        # TODO: Read object map file path into list of strings
+        # TODO: Return dictionary SquishObject.name:SquishObject converted from the list of strings
+        return None
+
+
 def main():
     parser = argparse.ArgumentParser(description='Reduces the Squish object map by removing duplicates and not used'
                                                  ' entries',
@@ -16,7 +47,10 @@ def main():
     # TODO: Validate arguments
     # TODO: Find all test files from specified folder
     # TODO: Extract all used objects from test files into SET_1
-    # TODO: Extract all objects from object map into SET_2
+
+    objects_extractor = ObjectMapExtractor(args.o)
+    object_map_objects = objects_extractor.extract_objects()
+
     # TODO: Create SET_3 containing objects from SET_2 which are part of SET_1 OR are referenced by objects in SET_2
 
 
