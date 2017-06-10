d7dbbcbf9053193e611055c3e24f0a92101d0a73
scais <stepan.cais@gmail.com>
2017-06-10 19:04:24 +0100

    Added more unit tests

--- map_reducer_test.py
+++ map_reducer_test.py
@@ -35,5 +35,66 @@ class TestSquishObject(unittest.TestCase):
         self.assertEqual(squish_object.attributes[1].value, 'brr')
         self.assertEqual(squish_object.attributes[1].matching_type, '=')
 
+    def test_parse_object_with_special_chars_in_name(self):
+
+        line = ":Bla.123.=-`]__[#;/.,{}()	{bar='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Bla.123.=-`]__[#;/.,{}()')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+    def test_parse_object_with_attribute_name_containing_dots(self):
+
+        line = ":Foo	{wo.lo.lo.loo='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'wo.lo.lo.loo')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
+    def test_parse_object_with_attribute_wildcard(self):
+
+        line = ":Foo	{Bar?='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '?=')
+
+    def test_parse_object_with_attribute_regex(self):
+
+        line = ":Foo	{Bar~='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '~=')
+
+    def test_parse_object_with_special_chars_in_attribute_value(self):
+
+        line = ":Foo	{Bar='.123.=-][#;/.,{}()__'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, ".123.=-][#;/.,{}()__")
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
 if __name__ == '__main__':
     unittest.main()
