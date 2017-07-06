cd42c6daa294a0c2d6aa87903e6f53822811f12f
scais <stepan.cais@gmail.com>
2017-07-06 21:34:58 +0100

    Added another test (currently failing)

--- map_reducer_test.py
+++ map_reducer_test.py
@@ -1,140 +1,148 @@
 import unittest
+import tempfile
 
 from map_reducer import SquishObject, ObjectMapExtractor, ValidFilesArranger
 
 
-# class TestSquishObject(unittest.TestCase):
-#
-#     def test_parse_object_with_one_attribute(self):
-#
-#         line = ":Foo	{bar='buz'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'bar')
-#         self.assertEqual(squish_object.attributes[0].value, 'buz')
-#         self.assertEqual(squish_object.attributes[0].matching_type, '=')
-#
-#     def test_parse_object_with_two_attributes(self):
-#
-#         line = ":Foo	{bar='buz' bux='brr'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'bar')
-#         self.assertEqual(squish_object.attributes[0].value, 'buz')
-#         self.assertEqual(squish_object.attributes[0].matching_type, '=')
-#
-#         self.assertEqual(squish_object.attributes[1].name, 'bux')
-#         self.assertEqual(squish_object.attributes[1].value, 'brr')
-#         self.assertEqual(squish_object.attributes[1].matching_type, '=')
-#
-#     def test_parse_object_with_special_chars_in_name(self):
-#
-#         line = ":Bla.123.=-`]__[#;/.,{}()	{bar='buz'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Bla.123.=-`]__[#;/.,{}()')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#     def test_parse_object_with_attribute_name_containing_dots(self):
-#
-#         line = ":Foo	{wo.lo.lo.loo='buz'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'wo.lo.lo.loo')
-#         self.assertEqual(squish_object.attributes[0].value, 'buz')
-#         self.assertEqual(squish_object.attributes[0].matching_type, '=')
-#
-#     def test_parse_object_with_attribute_wildcard(self):
-#
-#         line = ":Foo	{Bar?='buz'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'Bar')
-#         self.assertEqual(squish_object.attributes[0].value, 'buz')
-#         self.assertEqual(squish_object.attributes[0].matching_type, '?=')
-#
-#     def test_parse_object_with_attribute_regex(self):
-#
-#         line = ":Foo	{Bar~='buz'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'Bar')
-#         self.assertEqual(squish_object.attributes[0].value, 'buz')
-#         self.assertEqual(squish_object.attributes[0].matching_type, '~=')
-#
-#     def test_parse_object_with_special_chars_in_attribute_value(self):
-#
-#         line = ":Foo	{Bar='.123.=-][#;/.,{}()__'}"
-#
-#         squish_object = SquishObject.create_squish_object(line)
-#
-#         self.assertEqual(squish_object.name, 'Foo')
-#         self.assertTrue(len(squish_object.attributes), 1)
-#
-#         self.assertEqual(squish_object.attributes[0].name, 'Bar')
-#         self.assertEqual(squish_object.attributes[0].value, ".123.=-][#;/.,{}()__")
-#         self.assertEqual(squish_object.attributes[0].matching_type, '=')
-#
-#
-# class TestObjectMapExtractor(unittest.TestCase):
-#
-#     def test_extract_squish_objects_from_file(self):
-#
-#         with open('objects.map.sample') as file:
-#             lines = file.readlines()
-#
-#         squish_objects = ObjectMapExtractor(lines).extract_objects()
-#         self.assertTrue(len(squish_objects) > 0)
-#
-#     def test_handles_incorrect_lines(self):
-#
-#         with open('objects.map.sample') as file:
-#             lines = file.readlines()
-#
-#         squish_objects_orig = ObjectMapExtractor(lines).extract_objects()
-#
-#         lines.append("            ")
-#         lines.append(":Wololooo")
-#         lines.append(":Foo  FOO")
-#         lines.append(":Foo  FOO")
-#         lines.append(":Foo  {Bar=''}")
-#         lines.append(":Foo  {Bar='Buz' Bux=''}")
-#         lines.append(":Foo  {Bar='Buz' ''}")
-#
-#         squish_objects = ObjectMapExtractor(lines).extract_objects()
-#         self.assertTrue(len(squish_objects) == len(squish_objects_orig))
+class TestSquishObject(unittest.TestCase):
+
+    def test_parse_object_with_one_attribute(self):
+
+        line = ":Foo	{bar='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
+    def test_parse_object_with_two_attributes(self):
+
+        line = ":Foo	{bar='buz' bux='brr'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
+        self.assertEqual(squish_object.attributes[1].name, 'bux')
+        self.assertEqual(squish_object.attributes[1].value, 'brr')
+        self.assertEqual(squish_object.attributes[1].matching_type, '=')
+
+    def test_parse_object_with_special_chars_in_name(self):
+
+        line = ":Bla.123.=-`]__[#;/.,{}()	{bar='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Bla.123.=-`]__[#;/.,{}()')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+    def test_parse_object_with_attribute_name_containing_dots(self):
+
+        line = ":Foo	{wo.lo.lo.loo='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'wo.lo.lo.loo')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
+    def test_parse_object_with_attribute_wildcard(self):
+
+        line = ":Foo	{Bar?='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '?=')
+
+    def test_parse_object_with_attribute_regex(self):
+
+        line = ":Foo	{Bar~='buz'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, 'buz')
+        self.assertEqual(squish_object.attributes[0].matching_type, '~=')
+
+    def test_parse_object_with_special_chars_in_attribute_value(self):
+
+        line = ":Foo	{Bar='.123.=-][#;/.,{}()__'}"
+
+        squish_object = SquishObject.create_squish_object(line)
+
+        self.assertEqual(squish_object.name, 'Foo')
+        self.assertTrue(len(squish_object.attributes), 1)
+
+        self.assertEqual(squish_object.attributes[0].name, 'Bar')
+        self.assertEqual(squish_object.attributes[0].value, ".123.=-][#;/.,{}()__")
+        self.assertEqual(squish_object.attributes[0].matching_type, '=')
+
+
+class TestObjectMapExtractor(unittest.TestCase):
+
+    def test_extract_squish_objects_from_file(self):
+
+        with open('objects.map.sample') as file:
+            lines = file.readlines()
+
+        squish_objects = ObjectMapExtractor(lines).extract_objects()
+        self.assertTrue(len(squish_objects) > 0)
+
+    def test_handles_incorrect_lines(self):
+
+        with open('objects.map.sample') as file:
+            lines = file.readlines()
+
+        squish_objects_orig = ObjectMapExtractor(lines).extract_objects()
+
+        lines.append("            ")
+        lines.append(":Wololooo")
+        lines.append(":Foo  FOO")
+        lines.append(":Foo  FOO")
+        lines.append(":Foo  {Bar=''}")
+        lines.append(":Foo  {Bar='Buz' Bux=''}")
+        lines.append(":Foo  {Bar='Buz' ''}")
+
+        squish_objects = ObjectMapExtractor(lines).extract_objects()
+        self.assertTrue(len(squish_objects) == len(squish_objects_orig))
 
 
 class TestValidFilesArranger(unittest.TestCase):
 
     def test_empty_arg_list_returns_empty_list(self):
+
         valid_files_list = ValidFilesArranger('foo').prepare_valid_files([])
+
         self.assertTrue(len(valid_files_list) == 0)
 
     def test_existing_file_returns_list_size_one(self):
-        ValidFilesArranger('foo').prepare_valid_files(["", ""])
-        self.assertTrue(False)
+
+        temp_file = tempfile.NamedTemporaryFile(suffix='foo')
+
+        valid_files_list = ValidFilesArranger('foo').prepare_valid_files([temp_file.name])
+
+        self.assertTrue(len(valid_files_list) == 1)
+        self.assertTrue(valid_files_list[0] == temp_file.name)
 
     def test_existing_folder_with_one_file_returns_list_size_one(self):
         pass
